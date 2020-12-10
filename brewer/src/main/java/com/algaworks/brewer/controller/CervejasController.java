79146057e9d4ef9dfe106945109cc420f195e51c
julianosantos <julianosantos@localhost>
2020-12-10 18:22:39 -0300

    mensagens no layout e alteracao do arquivo de log

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -15,12 +15,16 @@ import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Origem;
 import com.algaworks.brewer.model.Sabor;
 import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.service.CadastroCervejaService;
 
 @Controller
 public class CervejasController {
 	
 	@Autowired
 	private Estilos estilos;
+	
+	@Autowired
+	private CadastroCervejaService cadastroCervejaService;
 
 	@RequestMapping("/cervejas/novo")
 	public ModelAndView novo(Cerveja cerveja) {
@@ -34,20 +38,12 @@ public class CervejasController {
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
-		/*if (result.hasErrors()) {
+		if (result.hasErrors()) {
 			return novo(cerveja);
-		}*/
+		}
 		
-		// Salvar no banco de dados...
+		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
-		System.out.println(">>> sku: " + cerveja.getSku());
-		System.out.println(">>> sabor: " + cerveja.getSabor());
-		System.out.println(">>> Origem: " + cerveja.getOrigem());
-		
-		System.out.println("cerveja.getEstilo(): " + cerveja.getEstilo());
-		if (cerveja.getEstilo() != null)
-			System.out.println(">>> Estilo: " + cerveja.getEstilo().getCodigo());
-		
 		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
