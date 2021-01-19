c2a8c17fcd0dce3bf7f347b5e5baa8eee477d7c5
Juliano Santos <foxports@hotmail.com>
2021-01-18 22:54:55 -0300

    - salvando e removendo formatacao do banco

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.algaworks.brewer.model.Cliente;
 import com.algaworks.brewer.model.TipoPessoa;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.CadastroClienteService;
 
 @Controller
 @RequestMapping("/clientes")
@@ -20,6 +21,8 @@ public class ClientesController {
 
 	@Autowired
 	private Estados estados;
+	
+	private CadastroClienteService cadastroClienteService;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Cliente cliente) {
@@ -35,7 +38,7 @@ public class ClientesController {
 			return novo(cliente);
 		}
 		
-		// TODO: Salvar e adicionar mensagem
+		cadastroClienteService.salvar(cliente);
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
 		return new ModelAndView("redirect:/clientes/novo");
 	}
